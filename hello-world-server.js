b2703d603e9d99ba89a3076c7e9875072703e2e8
yyh-gl <yhonda.95.gl@gmail.com>
2018-11-09 11:54:59 +0900

    コメント追加

--- hello-world-server.js
+++ hello-world-server.js
@@ -2,20 +2,19 @@ var express = require('express');
 var express_graphql = require('express-graphql');
 var { buildSchema } = require('graphql');
 
-// GraphQL schema
+// GraphQL schemaを設定
 var schema = buildSchema(`
  type Query {
  message: String
  }
 `);
 
-// Root resolver
+// Root resolverを設定
 var root = {
     message: () => 'Hello World!'
 };
 
-// Create an express server and a GraphQL endpoint
-
+// Expressサーバの初期化とGraphQLのエンドポイントを設定
 var app = express();
 app.use('/graphql', express_graphql({
     schema: schema,
